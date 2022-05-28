72bb5682227c57304eeee65b370eb883b57b504a
MalikovIslam <99679471+MalikovIslam@users.noreply.github.com>
2022-05-28 20:02:16 +0600

    Initial commit

---
+++ app/src/main/java/com/example/zametki/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.zametki;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.zametki.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, new MainFragment());
+        transaction.commit();
+
+
+    }
+
+}
